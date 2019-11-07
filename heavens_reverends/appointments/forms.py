482a46adad3238b72c7b0f76051905e1d973f2d2
drake hilliard <drake.hilliard@opcity.com>
2019-11-06 20:08:41 -0600

    add user appointments

--- heavens_reverends/appointments/forms.py
+++ heavens_reverends/appointments/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, DateTimeField, SelectField
+from wtforms import StringField, SubmitField, TextAreaField, DateTimeField, SelectField, IntegerField
 from wtforms.validators import DataRequired
+from datetime import date
 
 class AppointmentForm(FlaskForm):
     appointment_type = SelectField(label='Appointment Type',choices=['Wedding Ceremony','Something Else'], validators=[DataRequired()])
@@ -12,6 +13,10 @@ class WeddingAppointmentForm(FlaskForm):
     spouse_first_name = StringField('Spouse First Name', validators=[DataRequired()])
     spouse_last_name = StringField('Spouse Last Name', validators=[DataRequired()])
     # appointment_date = DateTimeField('Appointment Date', format='%m/%d/%y', validators=[DataRequired()])
+    month = IntegerField('Month', validators=[DataRequired()])
+    day = IntegerField('Day', validators=[DataRequired()])
+    year = IntegerField('Year', validators=[DataRequired()])
+    hour = IntegerField('Year', validators=[DataRequired()])
     submit = SubmitField('Book')
 
 # Could maybe combine this with above
--- heavens_reverends/appointments/views.py
+++ heavens_reverends/appointments/views.py
@@ -4,6 +4,7 @@ from flask_login import current_user,login_required
 from heavens_reverends import db
 from heavens_reverends.models import Appointment
 from heavens_reverends.appointments.forms import AppointmentForm, AppointmentUpdateForm, WeddingAppointmentForm
+from datetime import datetime
 
 appointments = Blueprint('appointments',__name__)
 
@@ -29,9 +30,14 @@ def book_wedding_appointment():
     appointment_type = 'wedding' # session.get('appointment_type', None)
 
     if form.validate_on_submit():
+        year = form.year.data
+        month = form.month.data
+        day = form.day.data
+        hour = form.hour.data
+        appointment_date = datetime(year,month,day,hour)
         appointment = Appointment(
                         appointment_type=appointment_type,
-                        appointment_date=form.appointment_date.data,
+                        appointment_date=appointment_date,
                         first_name=form.first_name.data,
                         last_name=form.last_name.data,
                         spouse_first_name=form.spouse_first_name.data,
@@ -48,5 +54,5 @@ def book_wedding_appointment():
 @appointments.route('/appointments/myappointments',methods=['GET','POST'])
 @login_required
 def user_appointments():
-    appointments = Appointments.query.filter_by(user_id=current_user.id)
+    appointments = Appointment.query.filter_by(user_id=current_user.id)
     return render_template('user_appointments.html',appointments=appointments)
